9456cedd83c83ac43a2af108dc73705e7c35378e
Schaum7 <falk.altrock@gmail.com>
2020-03-13 06:28:47 +0100

    added BMI calculator

--- main.py
+++ main.py
@@ -5,29 +5,41 @@ from ui.mainwindow import Ui_MainWindow
 
 app = QtWidgets.QApplication(sys.argv)
 
-window = QtWidgets.QMainWindow()
-window.setWindowTitle("Testanwendung")
 
+class MainWindow(QtWidgets.QMainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
 
-def on_button_click():
-    result = ""
+        self.setWindowTitle("Testanwendung")
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
 
-    if ui_window.le_input1.text() == "" or ui_window.le_input2.text() == "":
-        result = "FEHLER!! Eins der Felder ist leer!"
-    else:
-        if ui_window.le_input1.text() == ui_window.le_input2.text():
-            result = "GLEICH"
+        self.ui.pb_vergleiche.clicked.connect(self.on_pb_vergleiche_click)
+        self.ui.pb_berechne_bmi.clicked.connect(self.on_pb_berechne_bmi_click)
+
+    def on_pb_vergleiche_click(self):
+        result = ""
+
+        if self.ui.le_input1.text() == "" or self.ui.le_input2.text() == "":
+            result = "FEHLER!! Eins der Felder ist leer!"
+            self.ui.tb_result.setTextColor("red")
         else:
-            result = "UNGLEICH"
+            if self.ui.le_input1.text() == self.ui.le_input2.text():
+                result = "GLEICH"
+            else:
+                result = "UNGLEICH"
 
-    ui_window.tb_result.setText(result)
+        self.ui.tb_result.setText(result)
+
+    def on_pb_berechne_bmi_click(self):
+        groesse_m = float(self.ui.le_height_m.text())
+        gewicht_kg = float(self.ui.le_weight_kg.text())
+        bmi = gewicht_kg/(groesse_m**2)
+
+        self.ui.lbl_bmi.setText("BMI {0:.3f}".format(bmi))
 
 
-ui_window = Ui_MainWindow()
-ui_window.setupUi(window)
-ui_window.pb_vergleiche.clicked.connect(on_button_click)
-
-
+window = MainWindow()
 window.show()
 
 sys.exit(app.exec_())
